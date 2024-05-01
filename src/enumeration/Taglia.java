f67dd72245da0247fe9b149876e8c30cf8a9f26e
Jonny <81505820+Tony-jdl@users.noreply.github.com>
2024-05-01 00:28:52 -0700

    Sezione 14: Le Enumeration

--- src/enumeration/Taglia.java
+++ src/enumeration/Taglia.java
@@ -1,21 +1,9 @@
 package src.enumeration;
 
 public enum Taglia {
-    EXTRASMALL(1),
-    SMALL(2),
-    MEDIUM(3),
-    LARGE(4),
-    EXTRALARGE(5);
-
-    // in questo modo è possibile associare ad ogni enum un valore, come in questo esempio, di tipo numerico
-
-    private int valore;
-
-    Taglia(int valore){
-        this.valore = valore;
-    }
-
-    public int getValore() {
-        return valore;
-    }
+    EXTRASMALL,
+    SMALL,
+    MEDIUM,
+    LARGE,
+    EXTRALARGE;
 }
--- src/enumeration/Test.java
+++ src/enumeration/Test.java
@@ -3,14 +3,9 @@ package src.enumeration;
 public class Test {
     public static void main(String[] args) {
         Taglia m = Taglia.MEDIUM;
-        Taglia s = Taglia.SMALL;
-
-        if(m == s)
-            System.out.println("m e s sono uguali");
-        else
-            System.out.println("m e s sono diversi");
-
-        Taglia t = Taglia.MEDIUM;
-        System.out.println(Taglia.MEDIUM.getValore());
+        int ord = m.ordinal();
+        System.out.println(ord);
+        int pos = m.compareTo(Taglia.LARGE);
+        System.out.println(pos);
     }
 }
