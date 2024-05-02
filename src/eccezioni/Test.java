d29ddcc7a24f78f19b1f3c1f217bddb5f29cdfc8
Jonny <81505820+Tony-jdl@users.noreply.github.com>
2024-05-01 23:33:06 -0700

    Sezione 16: La Gestione delle Eccezioni - Sollevare una Eccezione

--- src/eccezioni/Test.java
+++ src/eccezioni/Test.java
@@ -4,23 +4,21 @@ import java.io.IOException;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        //myMethid();
-
         int x = 5, y = 0, z = 0;
+
+//        if(y == 0)
+//            throw new ArithmeticException();
+//        else {
+//            z = x / y;
+//            System.out.println("Tutto ok, z vale " + z);
+//        }
 
         try {
             z = x / y;
-            System.out.println("Tutto ok, z vale: " + z);
+            System.out.println("Tutto ok, z vale " + z);
         } catch (ArithmeticException e) {
-            System.out.println("Divizione per zero!");
-        } finally {
-            System.out.println("Sono nel blocco finally");
+            System.out.println("Divisione per zero!");
+            throw e;
         }
-    }
-
-    public static void myMethid() throws IOException {
-        int x = 5, y = 0, z = 0;
-        z = x / y;
-        System.out.println("Tutto ok, z vale: " + z);
     }
 }
