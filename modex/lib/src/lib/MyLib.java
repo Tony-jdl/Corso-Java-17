2be8b14af5f4d02a80408a3125208fcb4a858f44
Jonny <81505820+Tony-jdl@users.noreply.github.com>
2024-05-04 00:44:28 -0700

    Sezione 21: I Moduli

---
+++ modex/lib/src/lib/MyLib.java
@@ -0,0 +1,7 @@
+package lib;
+
+public class MyLib {
+    public void myMethod(String s){
+        System.out.println("MyLib:myMethod:" + s);
+    }
+}
---
+++ modex/test/src/test/MyTest.java
@@ -0,0 +1,10 @@
+package test;
+
+import lib.MyLib;
+
+public class MyTest {
+    public static void main(String[] args) {
+        MyLib ml = new MyLib();
+        ml.myMethod("MyTest");
+    }
+}
